9e0e791b572d0cf29975ce531cd9db1dee3b46cb
ashishnewar <ashish_201800140@smit.smu.edu.in>
2020-10-15 22:04:33 +0530

    my first commit

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -9,7 +9,7 @@ export class TodoComponent implements OnInit {
 
   constructor() { }
 
-  public todoList : string[] = ['buy chocolate', 'buy milk', 'go for a walk with someone and enjoy'];
+  public todoList : string[] = ['buy Ram', 'buy Harddisk', 'Buy Pendrive'];
   ngOnInit(): void {
   }
 
